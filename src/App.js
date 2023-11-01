628296d8992d3c7a7f40500c9b77c9bc77fb09c6
jo-well29 <rjowel@gmail.com>
2023-11-01 12:02:46 -0400

    Under Construction added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Body from './components/Body'
+import UnderConstruction from './components/UnderConstruction';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Body />
+      {/* <Header />
+      <Body /> */}
+
+      <UnderConstruction />
 
 
       
---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/UnderConstruction'
+
+const UnderConstruction = () => {
+  return (
+    <div className="under-construction">
+      <h1>Under Construction</h1>
+      <p>We're working on something awesome. Please check back later!</p>
+    </div>
+  );
+}
+
+export default UnderConstruction;
